2c3d34decfb8e5b498b8bc09a0853577359ae10c
Bradley <bradleydeadfield@gmail.com>
2021-09-14 15:27:26 +0100

    Added password checker.

---
+++ app/password_checker.py
@@ -0,0 +1,10 @@
+def check(password):
+    with open('10000_common.txt', 'r') as file:
+        password_list = [line.strip() for line in file]
+
+    for item in password_list:
+        if item == password:
+            return True
+            break
+        else:
+            return False
