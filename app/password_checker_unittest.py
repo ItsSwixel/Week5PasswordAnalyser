82234f45bdaeeb841f96bea4ad5fcb506a95e21e
Bradley <bradleydeadfield@gmail.com>
2021-09-14 16:31:07 +0100

    Updated password checker unit test.

---
+++ app/password_checker_unittest.py
@@ -0,0 +1,13 @@
+import unittest
+import password_checker
+
+class MyTestCase(unittest.TestCase):
+    def test(self):
+        self.assertEqual(password_checker.password_check("password"), True)
+        self.assertEqual(password_checker.password_check("qazwsx"), True)
+        self.assertEqual(password_checker.password_check("tuxedo"), True)
+        self.assertEqual(password_checker.password_check("t3st!t3st"), False)
+        self.assertEqual(password_checker.password_check("n0t4p455W0rd~"), False)
+        self.assertEqual(password_checker.password_check("t3st!nG"), False)
+if __name__ == '__main__':
+    unittest.main()
--- unit_tests/password_checker_unittest.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from app import password_checker
-
-class MyTestCase(unittest.TestCase):
-    def test(self):
-        self.assertEqual(password_checker.password_check("password"), ["Password is contained within common password list."])
-
-if __name__ == '__main__':
-    unittest.main()
