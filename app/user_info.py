e2b9121442701b55691b4eac031daf3991846d66
Bradley <bradleydeadfield@gmail.com>
2021-09-15 10:52:28 +0100

    Updated user info.

--- app/user_info.py
+++ app/user_info.py
@@ -2,21 +2,44 @@ import datetime
 
 def get_userinfo():
 
-    first_name = input("Please enter your first name: ")
-    last_name = input("Please enter your last name: ")
+    while True:
+            first_name = input("Please enter your first name: ")
 
-    date = input("Please enter your date of birth in YYYY-MM-DD format: ")
-    year, month, day = map(int, date.split("-"))
-    birthday = datetime.date(year, month, day)
+            if len(first_name) > 20:
+                print("\n First name is too large! \n")
+            elif not first_name.isalpha():
+                print("\n First name should only contain characters A-Z! \n")
+            else:
+                break
+
+    while True:
+            last_name = input("Please enter your last name: ")
+
+            if len(last_name) > 20:
+                print("\n Last name is too large! \n")
+            elif not last_name.isalpha():
+                print("\n Last name should only contain characters A-Z! \n")
+            else:
+                break
+
+    while True:
+        try:
+            date = input("Please enter your date of birth in YYYY-MM-DD format: ")
+            year, month, day = map(int, date.split("-"))
+            birthday = datetime.date(year, month, day)
+            break
+        except ValueError:
+            print("\n Wrong date format! \n")
 
     password = input("Please enter your password: ")
 
     return first_name, last_name, birthday, password
 
+
 if __name__ == '__main__':
 
     first_name, last_name, birthday, password = get_userinfo()
 
-    print("Your name: ", first_name, last_name)
+    print("\nYour name: ", first_name, last_name)
     print("Your birthday: ", birthday)
     print("Your password: ", password)
