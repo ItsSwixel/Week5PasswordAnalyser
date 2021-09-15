18f4c322346d7f0988190c72ce21d9cb413059d6
Bradley <bradleydeadfield@gmail.com>
2021-09-15 13:42:05 +0100

    Removed user info unit test.

--- app/user_info_unittest.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import user_info
-
-class MyTestCase(unittest.TestCase):
-    def test(self):
-        self.assertEqual(user_info.get_userinfo())
-
-if __name__ == "__main__":
-    unittest.main()
