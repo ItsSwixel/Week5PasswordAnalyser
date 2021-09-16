c1c00607cf2609b8eab442e491e120736e97f6b6
Lewis <itslink957@gmail.com>
2021-09-16 05:17:00 -0400

    Added Dockerfile for building and changed 1000_common.txt open so that it works within a container

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM python:3
+COPY app /app
+CMD ["python", "app/OOP_main.py"]
--- app/password_checker.py
+++ app/password_checker.py
@@ -1,6 +1,8 @@
-def password_check(password):
+import os
 
-    with open("10000_common.txt", "r") as file:
+
+def password_check(password):
+    with open(os.path.join(os.path.dirname(__file__), "10000_common.txt"), "r") as file:
         password_list = [line.strip() for line in file]
 
     for item in password_list:
